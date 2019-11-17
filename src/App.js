0aca7bfa06c82b4258d2fe1896e962f291d5351e
memo <mohammedfouadhassen@gmail.com>
2019-11-17 19:52:34 +0800

    App working after intialized

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>welcome to Currency Converter App</h1>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Here to start
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
